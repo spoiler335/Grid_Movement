529f36581a3a278c5ab97a1b80b64ed699db0f60
spoiler335 <54944126+spoiler335@users.noreply.github.com>
2022-09-12 12:42:06 +0530

    Updated Grid And Player Movement

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -4,35 +4,40 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] Sprite sprite;
-    float[,] a;
-    int vertical,horizontal,columns,rows;
+    [SerializeField] int rows;
+    [SerializeField] int columns;
+
+    [SerializeField] float tileSize =1;
+
+    [SerializeField] GameObject referenceTile;
 
     void Start()
     {
-        vertical = (int) Camera.main.orthographicSize;
-        horizontal = vertical - (Screen.width / Screen.height);
-        columns = horizontal * 2;
-        rows = vertical * 2;
-        a = new float[columns,rows];
-        for(int i = 0; i < columns; ++i)
-        {
-            for(int j = 0; j < rows; ++j)
-            {
-                a[i, j] = Random.Range(0f,1f);
-                generateGrid(i,j,a[i,j]);
-            }
-        }
+        generateGrid();   
     }
 
     
-    void generateGrid(int x, int y, float value)
+    void generateGrid()
     {
-        GameObject g = new GameObject("x"+x+"y"+y);
-        g.transform.position = new Vector3(x-(horizontal-0.5f),y-(vertical-0.5f));
-        var s = g.AddComponent<SpriteRenderer>();
-        s.sprite = sprite;
-        s.color = new Color(value,value,value);
+        
+        for(int i=0; i < rows; i++)
+        {
+            for(int j=0; j < columns; j++)
+            {
+                GameObject tile = (GameObject) Instantiate(referenceTile, transform);
+                var s= tile.GetComponent<SpriteRenderer>();
+                float r= Random.Range(0.0f, 1.0f);
+                s.color = new Color(r, r, r);
+                float posx = j * tileSize;
+                float posy = i * -tileSize;
+                tile.transform.position = new Vector3(posx, posy);
+            }
+        }
+
+        float gridW = columns * tileSize;
+        float gridH = rows * tileSize;
+        transform.position = new Vector2(  (-gridW/2 + tileSize) , (gridH/2 + tileSize) );
+        
 
     }
 
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,86 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    [SerializeField] float boundUp;
+    [SerializeField] float boundDown;
+    [SerializeField] float boundRight;
+    [SerializeField] float boundLeft;
+    [SerializeField] float speed = 1f;
+    private Swipe swipeControls;
+    private RectTransform rt;
+
     void Start()
+    {
+        swipeControls = GetComponent<Swipe>();
+        rt = GetComponent<RectTransform>();
+        transform.rotation = Quaternion.Euler(0,0,0);
+        transform.position = new Vector3(489,1142,0);     
+
+    }
+
+    void Update()
+    {
+        
+        //StartCoroutine(Move(direction));
+        transform.position += transform.up*speed*Time.deltaTime; 
+        
+        if(swipeControls.SwipeLeft)
+        {
+            transform.rotation = Quaternion.Euler(0,0,90);
+            Debug.Log("Left");
+        }
+
+        if(swipeControls.SwipeRight)
+        {
+            transform.rotation = Quaternion.Euler(0,0,-90);
+            Debug.Log("Right");
+        }
+
+        if(swipeControls.SwipeDown)
+        {
+            transform.rotation = Quaternion.Euler(0,0,180);
+            Debug.Log("Down");
+        }
+
+        if(swipeControls.SwipeUp)
+        {
+            transform.rotation = Quaternion.Euler(0,0,0);
+            Debug.Log("Up");
+        }
+
+        //checkBounds
+        if(rt.position.y > boundUp)
+        {
+            rt.position = new Vector3(transform.position.x, boundDown, transform.position.z);
+            Debug.Log(boundDown);
+        }
+
+        if(rt.position.y < boundDown)
+        {
+            rt.position = new Vector3(transform.position.x, boundUp, transform.position.z);
+            Debug.Log(boundUp);
+        }
+
+        if(rt.position.x > boundRight)
+        {
+            rt.position =  new Vector3(boundLeft, transform.position.y, transform.position.z);
+            Debug.Log(boundLeft);
+        }
+
+        if(rt.position.x < boundLeft)
+        {
+            rt.position =  new Vector3(boundRight, transform.position.y, transform.position.z);
+            Debug.Log(boundRight);
+        }
+        
+        
+    }
+
+    void checkBounds()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
