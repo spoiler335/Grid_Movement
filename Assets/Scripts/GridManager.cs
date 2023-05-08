b488696fc7664a58b1f39dba28a5975ff004dafc
spoiler335 <54944126+spoiler335@users.noreply.github.com>
2023-05-08 21:35:51 +0530

    Generating RGB Grid

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -7,27 +7,26 @@ public class GridManager : MonoBehaviour
     [SerializeField] int rows;
     [SerializeField] int columns;
 
-    [SerializeField] float tileSize =1;
+    [SerializeField] float tileSize = 1;
 
     [SerializeField] GameObject referenceTile;
 
     void Start()
     {
-        generateGrid();   
+        generateGrid();
     }
 
-    
+
     void generateGrid()
     {
-        
-        for(int i=0; i < rows; i++)
+
+        for (int i = 0; i < rows; i++)
         {
-            for(int j=0; j < columns; j++)
+            for (int j = 0; j < columns; j++)
             {
-                GameObject tile = (GameObject) Instantiate(referenceTile, transform);
-                var s= tile.GetComponent<SpriteRenderer>();
-                float r= Random.Range(0.0f, 1.0f);
-                s.color = new Color(r, r, r);
+                GameObject tile = (GameObject)Instantiate(referenceTile, transform);
+                var s = tile.GetComponent<SpriteRenderer>();
+                s.color = new Color32((byte)Random.Range(0, 256), (byte)Random.Range(0, 256), (byte)Random.Range(0, 256), 255);
                 float posx = j * tileSize;
                 float posy = i * -tileSize;
                 tile.transform.position = new Vector3(posx, posy);
@@ -36,11 +35,11 @@ public class GridManager : MonoBehaviour
 
         float gridW = columns * tileSize;
         float gridH = rows * tileSize;
-        transform.position = new Vector2(  (-gridW/2 + tileSize) , (gridH/2 + tileSize) );
-        
+        transform.position = new Vector2((-gridW / 2 + tileSize), (gridH / 2 + tileSize));
+
 
     }
 
-    
- 
+
+
 }
